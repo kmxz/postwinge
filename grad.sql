499b83c87acf358a3d9fad9764ed43a84bc7a517
kmxz <kxzkxz7139@gmail.com>
2015-04-06 23:35:26 +0800

    change column name

--- grad.sql
+++ grad.sql
@@ -1,46 +1,40 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
-CREATE TABLE IF NOT EXISTS `post_free` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `post_free`;
+CREATE TABLE `post_free` (
+  `post_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) unsigned NOT NULL,
   `reply_to` mediumint(8) unsigned NOT NULL,
   `image` tinytext CHARACTER SET ascii,
   `x_coord` smallint(6) NOT NULL,
   `y_coord` smallint(6) NOT NULL,
   `datetime` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`post_id`),
   UNIQUE KEY `coord` (`x_coord`,`y_coord`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
-CREATE TABLE IF NOT EXISTS `post_free_revision` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `post_free_revision`;
+CREATE TABLE `post_free_revision` (
+  `revision_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` mediumint(8) unsigned NOT NULL,
   `datetime` datetime NOT NULL,
   `text_content` text CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`revision_id`),
   KEY `post_id` (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
-CREATE TABLE IF NOT EXISTS `session` (
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
   `key` char(32) CHARACTER SET ascii NOT NULL,
   `user_id` smallint(5) unsigned NOT NULL,
   PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` smallint(6) NOT NULL AUTO_INCREMENT,
   `token` varchar(31) CHARACTER SET ascii NOT NULL,
   `display` tinytext CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
--- php/list.php
+++ php/list.php
@@ -6,13 +6,52 @@ if (isset($_GET['post_id'])) {
   // all revisions of a specific post
   $id = intval($_GET['post_id']);
   // we don't actually check whether the post exists, as it will not harm anyway
-  $stmt = $mysqli->prepare('SELECT * FROM `post_free_revision` WHERE `post_id` = ?');
+  $stmt = $mysqli->prepare('SELECT `revision_id`, `datetime`, `content` FROM `post_free_revision` WHERE `post_id` = ?');
   $stmt->bind_param('i', $id);
   if (!$stmt->execute()) { panic('SQL Error!'); }
   success($stmt->get_result()->fetch_all(MYSQLI_ASSOC));
 } else {
   // lastest revision for each post
-  $result = $mysqli->query('SELECT * FROM `post_free` LEFT JOIN (SELECT r1.* FROM `post_free_revision` r1 LEFT JOIN `post_free_revision` r2 ON (r1.`post_id` = r2.`post_id` AND r1.`id` < r2.`id`) WHERE r2.`id` IS NULL) mr ON `post_free`.`id` = mr.`post_id`');
+  $result = $mysqli->query('
+    SELECT
+      post.`post_id`,
+      post.`reply_to`,
+      post.`x_coord`,
+      post.`y_coord`,
+      post.`image`,
+      post.`user_id`,
+      post.`display`,
+      revision.`datetime`,
+      revision.`text_content`
+    FROM
+      (
+        SELECT
+          `post_free`.`post_id`,
+          `post_free`.`reply_to`,
+          `post_free`.`x_coord`,
+          `post_free`.`y_coord`,
+          `post_free`.`image`,
+          `post_free`.`user_id`,
+          `user`.`display`
+        FROM
+          `post_free`
+          LEFT JOIN
+          `user`
+          ON `post_free`.`user_id` = `user`.`user_id`
+      ) post
+      LEFT JOIN
+      (
+        SELECT
+          r1.*
+        FROM
+          `post_free_revision` r1
+          LEFT JOIN
+          `post_free_revision` r2
+          ON (r1.`post_id` = r2.`post_id` AND r1.`revision_id` < r2.`revision_id`)
+          WHERE r2.`revision_id` IS NULL
+      ) revision
+      ON post.`post_id` = revision.`post_id`
+  ');
   if (!$result) {
     panic('SQL Error!');
   }
